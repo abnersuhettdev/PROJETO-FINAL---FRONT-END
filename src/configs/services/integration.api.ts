6943928843d2c5b55b01f62f64b7f6a7e1230285
Abner Suhett <suhettabner@gmail.com>
2023-08-12 20:02:30 -0300

    adding api route

--- src/configs/services/integration.api.ts
+++ src/configs/services/integration.api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const serviceAPI = axios.create({
-	baseURL: import.meta.env.VITE_API_URL,
+	baseURL: 'https://api-notes-cft2.onrender.com',
+	// baseURL: import.meta.env.VITE_API_URL,
 });
 
 export default serviceAPI;
